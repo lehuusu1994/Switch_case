557c48cd027b0a7c5e87a40fe7cf1ab17619fc92
unknown <leesu031994@gmail.com>
2023-09-13 16:02:38 +0700

    Switch_case

---
+++ main.js
@@ -0,0 +1,31 @@
+function checkMonth() {
+  let month = +document.getElementById("month").value;
+  switch (month) {
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+      document.getElementById("result").innerHTML =
+        "Tháng " + month + " có 31 ngày.";
+
+      break;
+    case 2:
+      document.getElementById("result").innerHTML =
+        "Tháng 2 có 28 hoặc 29 ngày";
+      break;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+      document.getElementById("result").innerHTML =
+        "Tháng " + month + " Có 30 ngày";
+      break;
+
+    default:
+      document.getElementById("result").innerHTML = "Vui lòng nhập lại !";
+      break;
+  }
+}
